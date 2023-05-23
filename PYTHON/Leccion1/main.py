b7fc0107d154c4e46a53a7e472f702bff1842e5f
romirodrigez13@gmail.com <romirodrigez13@gmail.com>
2023-05-23 16:30:49 -0300

    nuevos comandos

--- PYTHON/Leccion1/main.py
+++ PYTHON/Leccion1/main.py
@@ -1,6 +1,61 @@
 miVariable = 3
 print(miVariable)
-miVariable = "a"
+miVariable = "Hola a todos los estudiantes de la tecnicatura"
 print(miVariable)
-miVariable =3.5
+miVariable = 3.5
 print(miVariable)
+x = 10
+y = 2
+z = x + y
+print(id(x))
+# Las literales se escriben con x344, la variable y = x088, la variable z = x408
+print(id(y))
+print(id(z))
+
+a = False
+print(type(a))
+# Tips int, float, bool
+x = 10
+print(x)
+print(type(x))
+x = 14.5
+print(x)
+print(type(x))
+x = "Hola alumnos"
+print(x)
+print(type(x))
+x = True
+print(x)
+print(type(x))
+x = False
+print(x)
+print(type(x))
+
+# Manejo de cadenas(String)
+miGrupoFavorito = "Divididos"
+caracteristicas = "La mejor banda de rock"
+print("Mi grupo favorito es:" + miGrupoFavorito, caracteristicas)
+
+numero1 = "7"
+numero2 = "8"
+print(int(numero1) + int(numero2))
+
+# Tipos de Boleanos(bool)
+miBoleano = 1 > 2
+print(miBoleano)
+
+if miBoleano:
+    print("El resultado es verdadero")
+else:
+    print("El resultado es falso")
+
+# Procesar la entrada del usuario
+# función input
+resultado = input("digite un numero: ")  # regresa un dato tipo string
+print(resultado)
+
+# Conversión de la entrada de datos
+numero1 = int(input("Escribe el primer numero: "))
+numero2 = int(input("Escribe el segundo numero: "))
+resultado = numero1 + numero2
+print("El resultado de la suma es: ", resultado)
