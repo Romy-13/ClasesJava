04739161cdd6f1b7e8f84c7f92acc7970a91ec80
romirodrigez13@gmail.com <romirodrigez13@gmail.com>
2023-05-30 15:46:40 -0300

    Java Leccion1

---
+++ JAVA/Lección1/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,30 @@
+
+public class HolaMundo {
+    public static void main(String[] args) {
+      /*  System.out.println("Hola mundo java"); 
+        
+        int miVariable = 10;
+        System.out.println(miVariable);
+        miVariable = 5;
+        System.out.println(miVariable);
+        //Tipo String
+        String miVariableCadena = "Bienvenidos";
+        System.out.println(miVariableCadena);
+        miVariableCadena = "Sigamos creciendo en programación";
+        System.out.println(miVariableCadena);
+        */
+      
+      //Var - inferencia de tipo en java
+      var miVariableEntera2 = 10;
+      var miVariableCadena2 = "Seguimos estudiando";
+      System.out.println("miVariableEntera2 = " + miVariableEntera2);
+      System.out.println("miVariableCadena2 = " + miVariableCadena2);
+      //souyv + tab
+      //Para ejecutar shift + F6 la tecla para mayúscula
+      //Regla para definir una variable en Java
+      
+      var miVariableEjemplo = 45;
+      
+    }
+    
+}
