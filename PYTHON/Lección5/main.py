4b1650c893a61a5e72fa7c2f7584aff1f804372b
romirodrigez13@gmail.com <romirodrigez13@gmail.com>
2023-10-02 09:31:16 -0300

    Clase 5 de Python Funciones y Ejercicios

---
+++ PYTHON/Lección5/main.py
@@ -0,0 +1,8 @@
+# Comenzamos con FUNCIONES
+# mi_funcion() # NO se puede llamar a una función antes de ser creada
+# Definimos una función
+def mi_funcion(): # Para identificar una función lleva paréntesis
+    print("Saludos a todos los alumnos de la Tecnicatura")
+
+mi_funcion() # Estamos llamando a la función creada
+mi_funcion() # Se puede llamar N veces a una función
