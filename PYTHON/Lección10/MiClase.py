16c8bf022f2e7fa629e74bf6546dc2cb7a95a37c
romirodrigez13@gmail.com <romirodrigez13@gmail.com>
2024-04-20 22:07:05 -0300

    Lección 10, Variables de clase, Métodos estaticos

--- PYTHON/Lección10/MiClase.py
+++ PYTHON/Lección10/MiClase.py
@@ -5,6 +5,10 @@ class MiClase:
     def __init__(self, variable_instancia):
         self.variable_instancia = variable_instancia
 
+    @staticmethod
+    def metodo_estatico(): #Metodo estatico
+        print(MiClase.variable_clase)
+
 print(MiClase.variable_clase)
 miClase1 = MiClase('Esta es una variable de instancia')
 print(miClase1.variable_instancia)
@@ -12,3 +16,11 @@ print(miClase1.variable_clase)
 miClase2 = MiClase('Esta es una prueba de variables')
 print(miClase2.variable_instancia)
 print(miClase2.variable_clase)
+
+MiClase.variable_clase2 = "Valor de la variable de clase 2"
+print(MiClase.variable_clase2)
+print(miClase1.variable_clase2)
+print(miClase2.variable_clase2)
+
+MiClase.metodo_estatico()
+
