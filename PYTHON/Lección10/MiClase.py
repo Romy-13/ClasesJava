f81c5b2d40cfc5eefb7bb82ab27cd7ecd7dfeca4
romirodrigez13@gmail.com <romirodrigez13@gmail.com>
2024-04-24 09:24:50 -0300

    clase 2 POO, Diseño, metodos estaticos

--- PYTHON/Lección10/MiClase.py
+++ PYTHON/Lección10/MiClase.py
@@ -6,8 +6,18 @@ class MiClase:
         self.variable_instancia = variable_instancia
 
     @staticmethod
-    def metodo_estatico(): #Metodo estatico
+    def metodo_estatico():  # Metodo estatico
         print(MiClase.variable_clase)
+
+    @classmethod
+    def metodo_clase(cls):
+        print(cls.variable_clase)
+
+    def metodo_instacia(self):
+        self.metodo_clase()
+        self.metodo_estatico()
+        print(self.variable_clase)
+        print(self.variable_instancia)
 
 print(MiClase.variable_clase)
 miClase1 = MiClase('Esta es una variable de instancia')
@@ -24,3 +34,7 @@ print(miClase2.variable_clase2)
 
 MiClase.metodo_estatico()
 
+MiClase.metodo_clase()
+miObjeto1 = MiClase("variable de instancia")
+miObjeto1.metodo_clase()
+miObjeto1.metodo_instacia()
